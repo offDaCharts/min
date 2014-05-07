01604d7d12cbba6908ceb7d4ee3f411b48897132
Quin <rqthames@yahoo.com>
2014-05-07 16:48:12 -0700

    added path analysis logic into function

--- entities/minFunction.js
+++ entities/minFunction.js
@@ -1,6 +1,43 @@
+var error = require('../error')
+
 function MinFunction(parameters, body) {
+  var hasReturnStatment = function(body) {
+        var conditionals = [],
+            returnFlag = false
+
+        for(i in body.statements) {
+            if (body.statements[i].constructor.name = "ReturnStatement") { 
+                returnFlag = true
+                break
+            } else if (body.statements[i].constructor.name = "ConditionalStatement") {
+                conditionals.push(body.statements[i])
+            } else if (body.statements[i].body) {
+                if (hasReturnStatment(body.statements[i].body)) {
+                    returnFlag = true
+                    break
+                }
+            }
+        }
+
+        if (returnFlag = false) {
+            for(i in conditionals) {
+                if (hasReturnStatment(conditionals[i].body) && 
+                   (conditionals[i].elseBody && hasReturnStatment(conditionals[i].elseBody))) {
+                    returnFlag = true
+                    break
+                }
+            }
+        }
+
+        return returnFlag
+      }
+
   this.parameters = parameters
   this.body = body
+
+  // if(!hasReturnStatment(this.body)) {
+  //   error('Function does not have a return statement in all possible paths')
+  // }
 }
 
 MinFunction.prototype.toString = function () {
@@ -9,20 +46,11 @@ MinFunction.prototype.toString = function () {
 
 MinFunction.prototype.analyze = function (context) {
   var localContext = context.createChildContext()
-      hasReturnStatment = function(body) {
-        
-      }
-
-
+      
   this.parameters.forEach(function (parameter) {
     localContext.addVariable(parameter.id.lexeme, parameter)
   })
   this.body.analyze(localContext)
-
-  for (i in body.statements) {
-    console.log(body.statements[i].constructor.name)
-  }
-  console.log(body)
 }
 
 module.exports = MinFunction
