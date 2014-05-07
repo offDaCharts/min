0b58fe01a9946e55ead9958a44ad3dad186b56bc
Quin <rqthames@yahoo.com>
2014-05-07 14:44:05 -0700

    took out optimization option

--- min.js
+++ min.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 var argv = require('optimist')
-  .usage('$0 [-t] [-a] [-o] [-i] filename')
+  .usage('$0 [-t] [-a] [-i] filename')
   .boolean(['t','a','o','i'])
   .describe('t', 'show tokens after scanning then stop')
   .describe('a', 'show abstract syntax tree after parsing then stop')
-  .describe('o', 'do optimizations')
   .describe('i', 'generate and show the intermediate code then stop')
   .demand(1)
   .argv
