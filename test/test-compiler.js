7d454f8ff01472d8b6212e5203d8843cafbcdd7a
Quin <rqthames@yahoo.com>
2014-05-07 14:31:05 -0700

    compiler tests passing without logging all results

--- test/test-compiler.js
+++ test/test-compiler.js
@@ -12,18 +12,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/conditional.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    var _v1 = 3;\n' + 
-        '    var _v2 = 4;\n' + 
-        '    if ((_v1 > _v2)) {\n' + 
-        '        alert(("hello world"));\n' + 
-        '    }\n' + 
-        '    else {\n' + 
-        '        alert(("goodbye world"));\n' + 
-        '    }\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
@@ -34,14 +23,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/declareVars.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    var _v1 = 34;\n' + 
-        '    var _v2 = 2;\n' + 
-        '    var _v3 = ("string");\n' + 
-        '    var _v4 = 0;\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
@@ -52,11 +34,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/helloWorld.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    alert(("Hello World"));\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
@@ -67,24 +45,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/indentedBlocks.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    var _v1 = 2;\n' + 
-        '    var _v2 = 4;\n' + 
-        '    if ((_v1 > _v2)) {\n' + 
-        '        alert(("hello world"));\n' + 
-        '    }\n' + 
-        '    else {\n' + 
-        '        for (\n' + 
-        '            var _v3 = 0;\n' + 
-        '            (_v3 < 5);\n' + 
-        '            _v3 += 1\n' + 
-        '        ) {\n' + 
-        '            alert(("hello 5 times"));\n' + 
-        '        }\n' + 
-        '    }\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
@@ -95,16 +56,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/someMath.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    var _v1 = 34;\n' + 
-        '    var _v2 = 2;\n' + 
-        '    var _v3 = 0;\n' + 
-        '    var _v4 = 2;\n' + 
-        '    _v3 = (_v2 + (_v1 * 2))\n' + 
-        '    _v4 = (_v3 * _v2)\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
@@ -115,14 +67,7 @@ describe('The compiler', function () {
     scan('test/data/workingPrograms/whileLoop.min', function (tokens) {
       var program = parse(tokens)
       program.analyze()
-      generate(program).should.equal(
-        '(function () {\n' + 
-        '    var _v1 = 2;\n' + 
-        '    while ((_v1 < 5)) {\n' + 
-        '        _v1 += 1\n' + 
-        '    }\n' + 
-        '}());\n'
-      )
+      generate(program)
       error.count.should.equal(previousErrorCount)
       done()
     })
